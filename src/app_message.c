78b3a59d86c5e0c9ce57e2a9286f658119ea8cc3
jorsmm <jorsmm@gmail.com>
2016-02-08 00:10:01 +0100

    mejoras de aspecto

--- src/app_message.c
+++ src/app_message.c
@@ -1,7 +1,6 @@
 #include <pebble.h>
 	
 #define REPEAT_INTERVAL_MS 50
-#define DIALOG_CHOICE_WINDOW_MESSAGE "Jorge TV"
 #define MAX_BITMAPS 9
 
 #ifdef PBL_SDK_3
@@ -16,6 +15,7 @@ static ActionBarLayer *s_action_bar_layer;
 static GBitmap *s_icon_bitmap;
 
 static GBitmap* table[MAX_BITMAPS];
+static char* titles[MAX_BITMAPS/3];
 
 int offset=0;
 
@@ -39,7 +39,9 @@ void send_message(int status){
 static void update_action_bar_layer() {
   action_bar_layer_set_icon(s_action_bar_layer, BUTTON_ID_UP, table[offset*3]);
   action_bar_layer_set_icon(s_action_bar_layer, BUTTON_ID_SELECT, table[(offset*3)+1]);
-  action_bar_layer_set_icon(s_action_bar_layer, BUTTON_ID_DOWN, table[(offset*3)+2]);  
+  action_bar_layer_set_icon(s_action_bar_layer, BUTTON_ID_DOWN, table[(offset*3)+2]); 
+  text_layer_set_text(s_label_layer, titles[offset]);
+
 } 
 
 static void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -60,7 +62,7 @@ static void click_config_provider(void *context) {
   window_single_repeating_click_subscribe(BUTTON_ID_UP, REPEAT_INTERVAL_MS, increment_click_handler);
   window_single_repeating_click_subscribe(BUTTON_ID_DOWN, REPEAT_INTERVAL_MS, decrement_click_handler);
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler); 
-  window_long_click_subscribe(BUTTON_ID_SELECT, 700, select_long_click_handler, NULL);
+  window_long_click_subscribe(BUTTON_ID_SELECT, 400, select_long_click_handler, NULL);
 }
 
 
@@ -72,7 +74,7 @@ static void window_load(Window *window) {
   // Set up the status bar last to ensure it is on top of other Layers
   s_status_bar = status_bar_layer_create();
   layer_add_child(window_layer, status_bar_layer_get_layer(s_status_bar));
-  status_bar_layer_set_colors(s_status_bar, GColorLightGray, GColorBlack);
+  status_bar_layer_set_colors(s_status_bar, GColorGreen, GColorBlack);
 #endif
   
   s_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TVC);
@@ -85,12 +87,14 @@ static void window_load(Window *window) {
 
   const GEdgeInsets label_insets = {.top = 122, .right = ACTION_BAR_WIDTH, .left = ACTION_BAR_WIDTH / 3 };
   s_label_layer = text_layer_create(grect_inset(bounds, label_insets));
-  text_layer_set_text(s_label_layer, DIALOG_CHOICE_WINDOW_MESSAGE);
   text_layer_set_background_color(s_label_layer, GColorClear);
   text_layer_set_text_alignment(s_label_layer, GTextAlignmentCenter);
   text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(s_label_layer));
 
+  titles[0]="Volume";
+  titles[1]="TV Channels";
+  titles[2]="General";
   table[0] = gbitmap_create_with_resource(RESOURCE_ID_VOLUP);
   table[1] = gbitmap_create_with_resource(RESOURCE_ID_MUTE);
   table[2] = gbitmap_create_with_resource(RESOURCE_ID_VOLDOWN);
